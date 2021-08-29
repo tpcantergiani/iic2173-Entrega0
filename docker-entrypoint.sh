1646429eba9fcbe817ce60ce3b213ee438577a21
tomascantergiani <tomas.cantergiani@beetrack.com>
2021-08-29 19:59:30 -0400

    fix: db migrate

--- docker-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-docker-compose exec python /usr/local/bin/python flask db init
-docker-compose exec python /usr/local/bin/python flask db migrate
-docker-compose exec python /usr/local/bin/python flask db upgrade
